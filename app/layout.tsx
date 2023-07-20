d9f3a2d857134d539e8ef55b508f24c370c55711
Chris Hewell Garrett <chris@bitski.com>
2023-07-20 14:43:13 -0400

    Initial login screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Script from 'next/script'
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +14,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <Script type="module" src="https://unpkg.com/@splinetool/viewer@0.9.388/build/spline-viewer.js" />
+      </head>
+      <body>{children}</body>
     </html>
   )
 }
